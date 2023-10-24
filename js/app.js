b3669496b172d4b017e685e008f2930ffba75504
hossain <hossainmahmud.cse@gmail.com>
2023-10-25 04:34:43 +0600

    add testimonial part

--- js/app.js
+++ js/app.js
@@ -14,3 +14,26 @@ $('.count').counterUp({
   delay: 10,
   time: 3000
 });
+// counter part end
+// nav & tab slider
+$('.nav_tab_cards').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  dots:true,
+  centerMode: true,
+  centerPadding: "0px"
+})
+// nav & tab end
+// testimonial part start
+$('.testimonial_review_part').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  dots:true,
+})
+// testimonial part end
