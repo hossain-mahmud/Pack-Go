6dea47ecc4b0d2928d563020c08a9aa90bdcb7b3
hossain <hossainmahmud.cse@gmail.com>
2023-11-02 01:25:18 +0600

    responsive update

--- js/app.js
+++ js/app.js
@@ -6,7 +6,31 @@ $('.destination_slider').slick({
     autoplaySpeed: 2000,
     arrows: false,
     centerMode: true,
-    centerPadding: "0px"
+    centerPadding: "0px",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 // slider part end
 // counter part start
